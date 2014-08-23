67fcdd0da08529bfdd38448d9aeb469f8a833d4e
DBK <david@thekingsburys.com>
2014-08-23 11:30:13 -0400

    Backend Birth
    backend.php handles sqlite. userService started with getUser

---
+++ backend.php
@@ -0,0 +1,66 @@
+<?php
+// DEBUG SQLITE
+
+// $dbname='base';
+// if(!class_exists('SQLite3'))
+  // die("SQLite 3 NOT supported.");
+
+// $base=new SQLite3($dbname, 0666);
+// echo "SQLite 3 supported."; 
+
+
+//-----------------------------------------------
+// POST DATA
+//-----------------------------------------------
+# Parse payload
+$payload = file_get_contents('php://input');
+if ($payload) {
+	$payloadDecoded = json_decode($payload, true);
+	if (!$payloadDecoded) {
+		exit("Failed to decode payload");
+	}
+}
+else {
+	exit("No payload");
+}
+
+//-----------------------------------------------
+// DB
+//-----------------------------------------------
+
+// unlink('sqlite.db');
+$db = new SQLite3('sqlite.db');
+
+$response = [];
+
+//-----------------------------------------------
+// METHODS
+//-----------------------------------------------
+
+if (isset($payloadDecoded['getuser'])) {
+
+  $userId = $payloadDecoded['getuser'];
+  
+  $stmt = $db->prepare('SELECT * FROM users WHERE id=:id');
+  $stmt->bindValue(':id', $userId, SQLITE3_INTEGER);
+  $r = $stmt->execute();
+  
+  $response['getUser'] = $r->fetchArray();
+}
+
+//-----------------------------------------------
+// RESPONSE AND EXIT
+//-----------------------------------------------
+echo json_encode($response);
+
+
+// stmt = $db->prepare('SELECT * FROM users WHERE id=:id');
+// $stmt->bindValue(':id', 1, SQLITE3_INTEGER);
+
+// $result = $stmt->execute();
+// var_dump($result->fetchArray());
+
+// $q = 
+
+
+?>
--- kingsbury.js
+++ kingsbury.js
@@ -1,1 +1,40 @@
 var kingsburyMod = angular.module('kingsburyMod', ['bootyApp']);
+
+kingsburyMod.run(function(userService) {
+
+  userService.getUser(1).then(function(user) {
+    console.log(user);
+  });
+
+});
+
+
+kingsburyMod.service('userService', function($log, $http, $q) {
+
+
+  this.GetUser = function(id) {
+  
+    var deferred = $q.defer();
+    
+    console.log('running getuser');
+    
+    $http.post('backend.php', {
+      'getuser': id
+    }).then(function(response) {
+      if (response.data) {
+        deferred.resolve(response.data);
+      }
+      else {
+        deferred.reject('Didn\'t get expected response');
+        $log.error('userService (getuser): Didn\'t get expected response');
+      }
+    });
+    
+    return deferred.promise;
+  }
+
+});
+
+
+kingsburyMod.service('inventoryService', function($log) {
+});
