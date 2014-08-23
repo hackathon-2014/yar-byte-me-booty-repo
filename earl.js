d7a33cd450aaa94133aebefd9f5da3661fd43bae
Bobby <bobby.earl@blackbaud.com>
2014-08-23 15:56:49 -0400

    Almost functioning Inventory Add

--- earl.js
+++ earl.js
@@ -1,8 +1,18 @@
 var earlMod = angular.module('earlMod', ['bootyApp']);
 
+earlMod.service('tmdbService', function($http) {
+  this.byTitle = function(title) {
+     return $http.get('//api.themoviedb.org/3/search/movie?api_key=39151834c95219c3cae772b4465079d7&query=' + title);
+  } 
+});
+
+/* NOPE
 earlMod.service('omdbService', function($http) {
   this.byTitle = function(title) {
-    return $http.get('//www.omdbapi.com/?tomatoes=false&s=' + title);
+    return $http.get('//www.omdbapi.com/?s=' + title);
+  }
+  this.byId = function(id) {
+    return $http.get('//omdbapi.api.com/?i=' + id); 
   }
 });
 
@@ -11,31 +21,51 @@ earlMod.service('imdbService', function($http) {
     return $http.get('proxy/?mode=raw&url=' + encodeURI('' + id));
   }
 });
+*/
 
-earlMod.controller('EarlController', function($scope, $http, omdbService) {
+earlMod.controller('InventoryAddController', function($scope, $http, tmdbService) {
   
-  $scope.omdbSearchByTitle = function(title) {
-    
-    $scope.response = $scope.error = '';
-    
-    imdbSearch.byTitle(title).success(function(data) {      
-      
-      if (typeof data.Error === 'string') {
-        $scope.error = data.Error; 
-      } else {
-        $scope.response = data;
+  var error = function() {
+    $scope.error = 'Error searching.';
+  };
+  
+  var success = function(r) {
+    console.log(r.data.results);
+    $scope.results = r.data.results;
+    for (var i = 0, j = $scope.results.length; i < j; i++) {
+      if ($scope.results[i].poster_path) {
+        $scope.results[i].poster_path = 'https://image.tmdb.org/t/p/w92' + $scope.results[i].poster_path;
       }
-      
-      $scope.$safeApply();
-      
-    }).error(function() {
-      $scope.imdbSearchError = 'Error searching';
-      $scope.$safeApply();
-   });
+    }
+  };
+
+  $scope.byTitle = function(title) {
+    $scope.isSearching = true;
+    $scope.requestId = setTimeout(function() {
+      $scope.results = $scope.error = '';
+      tmdbService.byTitle(title).then(success, error).finally(function() {
+        $scope.isSearching = false;
+        $scope.$safeApply();
+      });
+    }, 0);
+  }
+  
+  $scope.select = function(index) {
+    $scope.selected = $scope.results[index];
+  };
+  
+  $scope.unselect = function() {
+    $scope.selected = ''; 
+  }
+  
+  $scope.cancel = function() {
+    clearTimeout($scope.requestId);
+    $scope.isSearching = false;
   }
   
 });
 
+// http://stackoverflow.com/questions/15417125/submit-form-on-pressing-enter-with-angularjs
 earlMod.directive('ngEnter', function() {
   return function(scope, element, attrs) {
     element.bind("keydown keypress", function(event) {
@@ -43,7 +73,6 @@ earlMod.directive('ngEnter', function() {
         scope.$apply(function(){
           scope.$eval(attrs.ngEnter, {'event': event});
         });
-
         event.preventDefault();
       }
     });
