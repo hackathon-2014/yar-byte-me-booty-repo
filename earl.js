0b95637c78d72d913532f49cdda4142efee868a5
Bobby <bobby.earl@blackbaud.com>
2014-08-23 11:22:57 -0400

    Creating imdb service.

--- earl.js
+++ earl.js
@@ -1,1 +1,26 @@
 var earlMod = angular.module('earlMod', ['bootyApp']);
+
+earlMod.factory('imdbSearch', function($http) {
+
+  return {
+    byTitle : function(title) {
+      return $http.get('//www.omdbapi.com/?i=&t=' + title);
+    }
+  }
+  
+});
+
+earlMod.controller('EarlController', function($scope, $http, imdbSearch) {
+  $scope.imdbSearchByTitle = function(title) {
+    imdbSearch.byTitle(title).success(function(data) {
+      $scope.imdbSearchResults = data;
+      $scope.imdbSearchError = 'TEST';
+    }).error(function() {
+      $scope.imdbSearchError = 'Error searching';
+    });
+  }
+});
+
+earlMod.run(function($rootScope) {
+	
+});
